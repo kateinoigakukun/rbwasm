2ca48c668dc0ef37ac70bd2bf26a01eb693a7fed
Yuta Saito <kateinoigakukun@gmail.com>
2021-11-29 23:16:47 +0900

    initial commit

---
+++ build.rs
@@ -0,0 +1,23 @@
+use std::{env::var_os, path::Path, process::Command};
+
+fn main() {
+    let out_dir = var_os("OUT_DIR").unwrap();
+    let out_dir = Path::new(&out_dir);
+    build_libwasi_vfs_a(out_dir);
+}
+
+fn build_libwasi_vfs_a(out_dir: &Path) {
+    let src = Path::new("./wasi-vfs");
+    let target_dir = out_dir.join("wasi-vfs-target").canonicalize().unwrap();
+    let status = Command::new("cargo")
+        .current_dir(src)
+        .args(["build", "--target", "wasm32-unknown-unknown", "--release"])
+        .arg("--target-dir")
+        .arg(&target_dir)
+        .status()
+        .unwrap();
+    if !status.success() {
+        eprintln!("Failed building libwasi_vfs.a: {}", status);
+        std::process::exit(-1);
+    }
+}
