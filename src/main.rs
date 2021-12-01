ac36df2c48cd102b67f1e53fbc09c3d64b71278a
Yuta Saito <kateinoigakukun@gmail.com>
2021-12-01 16:28:24 +0900

    pass ruby build source by option

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::bail;
 use rbwasm::{
     asyncify_executable, build_cruby, link_executable, mkfs, toolchain, BuildSource, LinkerInput,
     MkfsInput, Workspace,
@@ -15,6 +16,40 @@ fn parse_map_dirs(s: &str) -> anyhow::Result<(PathBuf, PathBuf)> {
     Ok((parts[0].into(), parts[1].into()))
 }
 
+fn parse_build_src(s: &str) -> anyhow::Result<BuildSource> {
+    let mut kind_and_rests = s.split(":");
+    let kind = if let Some(kind) = kind_and_rests.next() {
+        kind
+    } else {
+        bail!("no build source kind");
+    };
+    let rest = kind_and_rests.collect::<Vec<_>>().join(":");
+    match kind {
+        "github" => {
+            let owner_and_rests = rest.split("/").collect::<Vec<_>>();
+            if owner_and_rests.len() != 2 {
+                bail!("invalid github pattern: only one / should appear");
+            }
+            let owner = owner_and_rests[0];
+            let repo_and_ref = owner_and_rests[1].split("@").collect::<Vec<_>>();
+            if repo_and_ref.len() != 2 {
+                bail!("invalid github pattern: only one @ should appear");
+            }
+            let repo = repo_and_ref[0];
+            let git_ref = repo_and_ref[1];
+            return Ok(BuildSource::GitHub {
+                owner: String::from(owner),
+                repo: String::from(repo),
+                git_ref: String::from(git_ref),
+            });
+        }
+        "path" => return Ok(BuildSource::Dir { path: rest.into() }),
+        other => {
+            bail!("unknown build source kind: {}", &other)
+        }
+    }
+}
+
 #[derive(StructOpt)]
 struct Opt {
     #[structopt(long = "mapdir", number_of_values = 1, value_name = "GUEST_DIR::HOST_DIR", parse(try_from_str = parse_map_dirs))]
@@ -28,6 +63,9 @@ struct Opt {
 
     #[structopt(long)]
     save_temps: bool,
+
+    #[structopt(long, default_value = "github:kateinoigakukun/ruby@834e125", parse(try_from_str = parse_build_src))]
+    cruby_src: BuildSource,
 }
 
 fn main() -> anyhow::Result<()> {
@@ -42,12 +80,7 @@ fn main() -> anyhow::Result<()> {
     }
     let workspace = Workspace::create(workspace_dir.canonicalize()?, opt.save_temps)?;
     let toolchain = toolchain::install_build_toolchain(&workspace)?;
-    let ruby_source = BuildSource::GitHub {
-        owner: String::from("kateinoigakukun"),
-        repo: String::from("ruby"),
-        git_ref: String::from("834e12525261d756da85b9b880dabe8407084902"),
-    };
-    let cruby = build_cruby(&workspace, &toolchain, &ruby_source)?;
+    let cruby = build_cruby(&workspace, &toolchain, &opt.cruby_src)?;
 
     let fs_object = if !opt.map_dirs.is_empty() {
         let input = MkfsInput {
@@ -66,4 +99,37 @@ fn main() -> anyhow::Result<()> {
     link_executable(&workspace, &toolchain, &cruby, &linker_input, &opt.output)?;
     asyncify_executable(&toolchain, &opt.output, &opt.output)?;
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::parse_build_src;
+
+    #[test]
+    fn parse_build_source_github() {
+        let src = parse_build_src("github:rust-lang/rust@main").expect("parse failed");
+        match src {
+            rbwasm::BuildSource::GitHub { owner, repo, git_ref } => {
+                assert_eq!(owner, "rust-lang");
+                assert_eq!(repo, "rust");
+                assert_eq!(git_ref, "main");
+            }
+            other => {
+                panic!("unexpected build source: {:?}", other);
+            }
+        }
+    }
+
+    #[test]
+    fn parse_build_source_path() {
+        let src = parse_build_src("path:../rust-lang/rust").expect("parse failed");
+        match src {
+            rbwasm::BuildSource::Dir { path } => {
+                assert_eq!(path.to_string_lossy(), "../rust-lang/rust");
+            }
+            other => {
+                panic!("unexpected build source: {:?}", other);
+            }
+        }
+    }
 }
